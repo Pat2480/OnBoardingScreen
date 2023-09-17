cdb935a29f66f1e6c5c8ac1d318b3011bc386896
Pat2480 <121413087+Pat2480@users.noreply.github.com>
2023-09-18 05:59:41 +0700

    Add files via upload

---
+++ lib/screens/onboarding_screen.dart
@@ -0,0 +1,228 @@
+//flutter pub add google_fonts
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class OnBoardingScreen extends StatefulWidget {
+  const OnBoardingScreen({super.key});
+
+  @override
+  State<OnBoardingScreen> createState() => _OnBoardingScreenState();
+}
+
+class _OnBoardingScreenState extends State<OnBoardingScreen> {
+  late PageController _pageController;
+  int _pageIndex = 0;
+
+  @override
+  void initState() {
+    _pageController = PageController(initialPage: 0);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(8, 0, 8, 20),
+          child: Column(
+            children: [
+              Expanded(
+                child: PageView.builder(
+                  itemCount: demo_data.length,
+                  controller: _pageController,
+                  onPageChanged: (index) {
+                    setState(() {
+                      _pageIndex = index;
+                    });
+                  },
+                  itemBuilder: (context, index) => OnBoardingWidget(
+                    image: demo_data[index].image,
+                    title: demo_data[index].title,
+                    description: demo_data[index].description,
+                  ),
+                ),
+              ),
+              Row(
+                children: [
+                  ...List.generate(
+                      demo_data.length,
+                      (index) => Padding(
+                            padding: const EdgeInsets.only(right: 4),
+                            child: DotIndicator(isActive: index == _pageIndex),
+                          )),
+                  //   const Spacer(),
+                  //   SizedBox(
+                  //   height: 60,
+                  //   width: 60,
+                  //   child: ElevatedButton(
+                  //     style: ElevatedButton.styleFrom(
+                  //         shape: const CircleBorder(),
+                  //         backgroundColor: Colors.pink),
+                  //     onPressed: () {
+                  //       _pageController.nextPage(
+                  //         duration: const Duration(microseconds: 300),
+                  //         curve: Curves.ease,
+                  //       );
+                  //     },
+                  //     child: const Icon(
+                  //       Icons.arrow_forward,
+                  //       color: Colors.white,
+                  //       size: 30,
+                  //     ),
+                  //   ),
+                  // ),
+
+                  //   Container(
+                  //     height: 12,
+                  //     width: 12,
+                  //     decoration: const BoxDecoration(
+                  //       color: Colors.red,
+                  //       borderRadius: BorderRadius.all(
+                  //         Radius.circular(12),
+                  //       )
+                  //     ),
+                  //   )
+                  // ],)
+
+                  const Spacer(),
+                  SizedBox(
+                    height: 60,
+                    width: 60,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          shape: const CircleBorder(),
+                          padding: EdgeInsets.zero,
+                          backgroundColor: Colors.pink),
+                      onPressed: () {
+                        _pageController.nextPage(
+                          duration: const Duration(microseconds: 300),
+                          curve: Curves.ease,
+                        );
+                      },
+                      child: const Icon(
+                        Icons.arrow_forward,
+                        color: Colors.white,
+                        size: 30,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class DotIndicator extends StatelessWidget {
+  const DotIndicator({
+    super.key,
+    required this.isActive,
+  });
+
+  final bool isActive;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 300),
+      height: isActive ? 16 : 4,
+      width: 4,
+      decoration: BoxDecoration(
+        color: isActive ? Colors.red : Colors.red.withOpacity(0.4),
+        borderRadius: const BorderRadius.all(Radius.circular(12)),
+      ),
+    );
+  }
+}
+
+class OnBoard {
+  final String image, title, description;
+
+  OnBoard({
+    required this.image,
+    required this.title,
+    required this.description,
+  });
+}
+
+final List<OnBoard> demo_data = [
+  OnBoard(
+      image: "assets/images/1.png",
+      title: "Welcome to Fresh Bakes",
+      description: "Discover delicious pastries, bread, and more."),
+  OnBoard(
+      image: "assets/images/2.png",
+      title: "Sip & Savor",
+      description: "Pair your treats with our finest coffee blends."),
+  OnBoard(
+      image: "assets/images/3.png",
+      title: "Easy Ordering",
+      description: "Order your favorites with just a few taps."),
+  OnBoard(
+      image: "assets/images/4.png",
+      title: "Swift Delivery",
+      description: "Enjoy quick and convenient doorstep delivery."),
+  OnBoard(
+      image: "assets/images/5.png",
+      title: "Join Our Community",
+      description: "Connect with fellow foodies and share your experiences."),
+];
+
+class OnBoardingWidget extends StatelessWidget {
+  const OnBoardingWidget({
+    super.key,
+    required this.image,
+    required this.title,
+    required this.description,
+  });
+
+  final String image, title, description;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const Spacer(),
+        Image.asset(
+          image,
+          height: 280,
+        ),
+        const Spacer(),
+        Text(
+          title,
+          textAlign: TextAlign.center,
+          style: GoogleFonts.lato(
+            textStyle: Theme.of(context).textTheme.displayLarge,
+            fontSize: 20,
+            fontWeight: FontWeight.w700,
+            fontStyle: FontStyle.italic,
+          ),
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        Text(
+          description,
+          textAlign: TextAlign.center,
+          style: GoogleFonts.lato(
+            textStyle: Theme.of(context).textTheme.displayLarge,
+            fontSize: 16,
+            fontWeight: FontWeight.w500,
+            fontStyle: FontStyle.italic,
+          ),
+        ),
+        const Spacer(),
+      ],
+    );
+  }
+}
